d66b18dd64dfd0f7cd9bb0947cdbf6cef72c7e09
martinbertel <98279215+martinbertel@users.noreply.github.com>
2022-05-06 13:15:58 -0400

    Add files via upload

---
+++ continue_break_exercise.py
@@ -0,0 +1,10 @@
+trasportation = ["train", "bus", "cab", "bike"]
+for i in trasportation:
+    if i == "cab":
+        continue
+    print(i)
+
+for i in range(100):
+    print(i)
+    if i == 98:
+        break
